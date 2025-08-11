cd1623f15c014b46ef53e596f43371c71c877b19
Olgu Dülger <olgudulger@gmail.com>
2025-08-11 17:39:06 +0300

    Tarla (FieldProperty) HasShareholder re-enable + snapshot ve pending migration ekle
    - FieldProperty.HasShareholder aktif
    - PropertyManager ve PropertyProfile mapping eklendi
    - EmlakDbContextModelSnapshot güncellendi
    - EnsureHasShareholderColumnSafe migration dahil
    
    Canlı: startup MigrateAsync + güvenli migration ile kolon otomatik oluşacak

--- DataAccess/Migrations/20250811142440_TemporarilyRemoveHasShareholderAgain.Designer.cs
+++ DataAccess/Migrations/20250811142440_TemporarilyRemoveHasShareholderAgain.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(EmlakDbContext))]
-    partial class EmlakDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250811142440_TemporarilyRemoveHasShareholderAgain")]
+    partial class TemporarilyRemoveHasShareholderAgain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
